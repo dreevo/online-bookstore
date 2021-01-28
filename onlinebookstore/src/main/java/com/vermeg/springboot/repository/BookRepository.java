fcf279f75a755a7ac5e8f4894c9b654885a38a96
moagharbi <moagharbi@TNLT1725.vermeg.com>
2021-01-28 04:42:18 +0100

    Added Template to the website with bootstrap shop template.

--- onlinebookstore/src/main/java/com/vermeg/springboot/repository/BookRepository.java
+++ onlinebookstore/src/main/java/com/vermeg/springboot/repository/BookRepository.java
@@ -1,12 +1,14 @@
 package com.vermeg.springboot.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.vermeg.springboot.entity.Book;
 
 /*
 Author : Gharbi Med Aziz
 */
+@CrossOrigin("http://localhost:4200")
 public interface BookRepository extends JpaRepository<Book, Long>{
 
 }
